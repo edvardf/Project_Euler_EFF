5fb7b928a45918b1cf80ab946447052a907365f6
Isak <isak@obsidian.no>
2020-11-07 23:53:20 +0100

    Solve problem 4

--- problem_4.py
+++ problem_4.py
@@ -5,3 +5,22 @@ The largest palindrome made from the product of two 2-digit numbers is 9009 = 91
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
 
+p = 0
+
+for i in range(999, 99, -1):
+    if i * i < p:
+        break
+    for j in range(i, 99, -1):
+        buf = num = i * j
+        r_num = 0
+
+        while buf != 0:
+            r_num *= 10
+            r_num += buf % 10
+            buf //= 10
+
+        if r_num == num and num > p:
+            p = num
+            break
+
+print(p)
