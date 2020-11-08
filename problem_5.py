9c9c45f9a6adf0d47c9b8750a97794acf5337e9b
Isak <isak@obsidian.no>
2020-11-08 17:00:19 +0100

    Edit problem 5 and solve problem 7

--- problem_5.py
+++ problem_5.py
@@ -4,19 +4,26 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
 
+from __future__ import annotations
+
 from sys import argv
 
-primes = list()
-lim = int(argv[1])
 
-for i in range(2, lim + 1):
-    prime = True
-    for j in range(2, i):
-        if i % j == 0:
-            prime = False
-            break
-    if prime:
-        primes.append(i)
+def primes_less_than(n : int) -> list[int]:
+    primes = list()
+    for i in range(2, n + 1):
+        prime = True
+        for j in range(2, i):
+            if i % j == 0:
+                prime = False
+                break
+        if prime:
+            primes.append(i)
+    return primes
+
+
+lim = int(argv[1])
+primes = primes_less_than(lim)
 
 powers = [0 for _ in primes]
 
---
+++ problem_7.py
@@ -0,0 +1,21 @@
+"""
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+"""
+
+primes = [2]
+i = primes[-1] + 1
+
+while len(primes) < 10001:
+    is_p = True
+    for p in primes:
+        if i % p == 0: 
+            is_p = False
+            break
+    if is_p:
+        primes.append(i)
+    else:
+        i += 1
+
+print(primes[-1])
