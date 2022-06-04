2a48a5e6d85afb03d1976a9be91a7412d4e9e23a
Edvard Fyksen <fyedvard@gmail.com>
2022-06-04 12:37:59 +0200

    Clean p024.py, start p026.py

--- p024.py
+++ p024.py
@@ -8,24 +8,17 @@ we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are
 
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 """
+from calendar import c
 from math import factorial
 
 
 def n_perm(n, e_l):
     perms = factorial(len(e_l))
-    d = []
+    d = e_l.copy()
     p = []
-    if n == 0:
-        return e_l
-    if n >= perms:
-        return "Error, n out of range"
     for i in range(len(e_l)):
         perms /= len(e_l)-i
         if n >= perms:
-            for j in range(i, len(e_l)):
-                d.append(e_l[j])
-            for j in range(i):
-                p.append(e_l[j])
             break
     for i in range(len(d)):
         p.append(d[int(n//perms)])
@@ -37,10 +30,10 @@ def n_perm(n, e_l):
     return p
 
 
-digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-ans_l = n_perm(10**6-1, digits)
-ans = 0
-for i in range(len(ans_l)):
-    c = 10 ** (len(ans_l) - i - 1)
-    ans += c * ans_l[i]
-print(ans)
+ans = n_perm(1e6-1, [0,1,2,3,4,5,6,7,8,9])
+x=0
+c = len(ans)-1
+for _ in n_perm(1e6-1, [0,1,2,3,4,5,6,7,8,9]):
+    x += 10**c*_
+    c -= 1
+print(x, ans)
