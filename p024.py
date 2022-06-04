0896d7172f5a66e08f66b22d8ae830df2eb4d80f
Edvard <mreddyfix@gmail.com>
2022-06-04 13:00:26 +0200

    clean som stuff

--- p024.py
+++ p024.py
@@ -8,7 +8,6 @@ we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are
 
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 """
-from calendar import c
 from math import factorial
 
 
@@ -17,23 +16,23 @@ def n_perm(n, e_l):
     d = e_l.copy()
     p = []
     for i in range(len(e_l)):
-        perms /= len(e_l)-i
+        perms /= len(e_l) - i
         if n >= perms:
             break
     for i in range(len(d)):
-        p.append(d[int(n//perms)])
-        d.pop(int(n//perms))
-        n -= perms*(n//perms)
+        p.append(d[int(n // perms)])
+        d.pop(int(n // perms))
+        n -= perms * (n // perms)
         if len(d) == 0:
             break
         perms /= len(d)
     return p
 
 
-ans = n_perm(1e6-1, [0,1,2,3,4,5,6,7,8,9])
-x=0
-c = len(ans)-1
-for _ in n_perm(1e6-1, [0,1,2,3,4,5,6,7,8,9]):
-    x += 10**c*_
+ans = n_perm(1e6 - 1, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+x = 0
+c = len(ans) - 1
+for _ in ans:
+    x += 10 ** c * _
     c -= 1
 print(x, ans)
