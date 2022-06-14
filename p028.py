8f4c7b7b128b53af4ebe2af3a09c41bee2ff10b8
Edvard Fyksen <fyedvard@gmail.com>
2022-06-15 00:55:45 +0200

    solve p028.py

--- p028.py
+++ p028.py
@@ -14,17 +14,44 @@ What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed 
 
 
 def clockwise_spiral(side_len: int):
-    nums = [n for n in range(1, side_len**2 + 1)]
-    arms = side_len * 4 - 3
-    spiral = [[] for arm in range(arms)]
-    arm_len = 1
-    for arm in range(arms):
-        if (arm - 1) % 3 == 0:
+    arm_count = side_len * 2 - 1
+    arms = [[] for arm in range(arm_count)]
+    arm_len = 0
+    for arm in range(arm_count):
+        if arm % 2 == 0:
+            c = 1 + arm_len * (arm_len + 1)
             arm_len += 1
+        else:
+            c = 1 + arm_len**2
         for i in range(arm_len):
-            spiral[arm].append(nums[i])
+            arms[arm].append(i + c)
+    spiral = [[0 for n in range(side_len)] for row in range(side_len)]
+    row_index = side_len // 2
+    if side_len % 2 == 0:
+        row_index -= 1
+    column_index = row_index
+    for i in range(arm_count):
+        direction = i % 4
+        for n in arms[i]:
+            spiral[row_index][column_index] = n
+            if direction == 0:  # right
+                column_index += 1
+            if direction == 1:  # down
+                row_index += 1
+            if direction == 2:  # left
+                column_index -= 1
+            if direction == 3:  # up
+                row_index -= 1
     return spiral
 
 
-for row in clockwise_spiral(5):
-    print(row)
+def square_matrix_diag_sum(sqaure_matrix):
+    ans = -1
+    for i in range(len(sqaure_matrix)):
+        ans += sqaure_matrix[i][i] + sqaure_matrix[-(i + 1)][i]
+    if len(sqaure_matrix) % 2 == 0:
+        ans += 1
+    return ans
+
+
+print(square_matrix_diag_sum(clockwise_spiral(1001)))
